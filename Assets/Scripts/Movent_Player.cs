058da4e65a1765c0a2dcdc8115a1b73d6db3c6b0
MichaaelVacca <michaelvacca07@gmail.com>
2022-04-04 14:56:54 -0400

    Fixed jumping issue

--- Assets/Scripts/Movent_Player.cs
+++ Assets/Scripts/Movent_Player.cs
@@ -20,9 +20,10 @@ public class Movent_Player : MonoBehaviour
     private Vector3 velocity;
     [SerializeField]
     private Camera characterCam;
-    private bool onGround = true;
+    bool onGround = true;
 
-    
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -36,17 +37,18 @@ public class Movent_Player : MonoBehaviour
     // Updat is called once per frame
     void Update()
     {
-        Debug.Log(onGround);
+
+
 
         if (Input.GetKey(KeyCode.A))
         {
             controller.Move(-transform.right * movementSpeed * Time.deltaTime);
-            
+
         }
         if (Input.GetKey(KeyCode.D))
         {
             controller.Move(transform.right * movementSpeed * Time.deltaTime);
-            
+
         }
         if (Input.GetKey(KeyCode.W))
         {
@@ -56,39 +58,43 @@ public class Movent_Player : MonoBehaviour
         if (Input.GetKey(KeyCode.S))
         {
             controller.Move(-transform.forward * movementSpeed * Time.deltaTime);
-            
-        }
-        
-        if (Input.GetKeyDown(KeyCode.Space) && controller.isGrounded)
-        {
-           
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravityPull);
-                
+
         }
 
-        Debug.Log(controller.isGrounded);
+        Jump();
 
-                
-        /*var ray = new Ray(transform.position, -transform.up);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, 1.5f))
-        {
-            onGround = true;
-            //*Debug.Log("on Ground" + onGround);*//*
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                velocity.y = Mathf.Sqrt(jumpHeight * 2 * gravityPull);
-                onGround = false;
-
-            }
-        }*/
-        //Constant downwards pull on the character 
         velocity.y += gravityPull * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
 
-        
+
 
     }
+
+    private void Jump()
+    {
+        var ray = new Ray(transform.position, Vector3.down); // shoots raycast down always 
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 1.5f)) // Check if ray cast hit an object. Distance of the ray cast down is 1.5 units
+        {
+            onGround = true; //if the ray cast hit something than it is on the ground
+
+        }
+        else
+        {
+            onGround = false;// Means it's in the air
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (onGround)//read the bool value from the rayCast
+            {
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravityPull);
+
+            }
+        }
+    }
+
     void Run()
     {
         if (Input.GetKey(KeyCode.LeftShift))
