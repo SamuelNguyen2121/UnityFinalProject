47594ed94ebc88830cb9b8515e47a7d6849ffc3f
SamuelNguyen2121 <2032373@champlaincollege.qc.ca>
2022-04-26 11:01:15 -0400

    added timer and modified menus

---
+++ Assets/Scripts/TimerScript.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{
+    [SerializeField]
+    Text score;
+
+    [SerializeField]
+    int time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(runTimer());
+    }
+
+    IEnumerator runTimer()
+    {
+        while (true)
+        {
+            score.text = time.ToString();
+            yield return new WaitForSeconds(1f);
+            time--;
+
+            if (time == 0) {
+                break;
+            }
+        }
+        LoadGameOver();
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+    public void LoadGameOver()
+    {
+        SceneManager.LoadScene("GameOverScene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
